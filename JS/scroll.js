c9811dd778b9e47eb3e87f4f8b2ce0eb50dd93bc
本荘俊介 <shun8kide@gmail.com>
2021-12-24 01:48:36 +0900

    津田沼PRファイル

---
+++ JS/scroll.js
@@ -0,0 +1,42 @@
+ScrollReveal().reveal('.fuwa',{
+  delay: 400,
+  viewFactor: 0.3,
+  // origin:bottom,
+});
+ScrollReveal().reveal('.fuwa2',{
+  delay: 600,
+  viewFactor: 0.3,
+  // origin:bottom,
+});
+ScrollReveal().reveal('.fuwa3',{
+  delay: 800,
+  viewFactor: 0.3,
+  // origin:bottom,
+});
+ScrollReveal().reveal('.fuwa4',{
+  delay: 1000,
+  viewFactor: 0.3,
+  // origin:bottom,
+});
+ScrollReveal().reveal('.fuwa5',{
+  duration: 2000,
+  scale: 0.5,
+  delay: 200,
+});
+ScrollReveal().reveal('.fuwa6',{
+  duration: 4000,
+  scale: 0.5,
+  delay: 600,
+});
+
+ScrollReveal().reveal('.main_head,.main_head_r,.main_head_m ', {
+  duration: 1600,
+  scale: 0.3,
+  mobile: 'false'
+});
+
+ScrollReveal().reveal('.sobu', {
+  duration: 3000,
+  origin: 'left',
+  distance: '300px',
+});
